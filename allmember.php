49fa3325a49b175add268e95b12758d9f9f930cb
qwe329295 <105021028@live.asia.edu.tw>
2018-01-10 13:50:19 +0800

    1/10

---
+++ allmember.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2018/1/10
+ * Time: 下午 12:33
+ */?>
+    <html lang="en">
+    <head>
+        <title>控制台</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+    <div class="container">
+        <h2>CONTROL YOUTSELF</h2>
+        <p>UPDATE ,DELETE OR MODIFY</p>
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">首頁</a></li>
+            <li><a data-toggle="tab" href="#menu1">重要訊息公告</a></li>
+            <li><a data-toggle="tab" href="#menu2">計算機組織</a></li>
+            <li><a data-toggle="tab" href="#menu3">微處理器系統</a></li>
+            <li><a data-toggle="tab" href="#menu4">無線網路</a></li>
+            <li><a data-toggle="tab" href="#menu5">多媒體網站技術</a></li>
+            <li><a data-toggle="tab" href="#menu6">嵌入式系統</a></li>
+            <li><a data-toggle="tab" href="#menu7">網站分析與管理</a></li>
+            <li><a data-toggle="tab" href="#menu8">著作</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active">
+                <h3>HOME</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="menu2" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+            <div id="menu3" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+            <div id="menu4" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+            <div id="menu5" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+            <div id="menu6" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+            <div id="menu7" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+            <div id="menu8" class="tab-pane fade">
+                <h3>Menu 3</h3>
+                <p></p>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    </body>
+    </html>
